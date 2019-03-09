79509a52ed02fe1c02b21d0056da017673e9275f
Lizhong Yu <114469538@qq.com>
2019-03-09 18:21:20 +0800

    Add files via upload

---
+++ Chapter2_Sorting/2.4 Priority Queues/Ex2_4_33_IndexPQImplementation.java
@@ -0,0 +1,12 @@
+package Chapter2_4_PriorityQueues;
+/*
+*   Index priority-queue implementation. Implement the basic operations in the index priority-queue API on page 320 by
+*   modifying Algorithm 2.6 as follows: Chnage pq[] to hold indices, add an array keys[] to hold the key values, and add
+*   an array qp[] that is the inverse of pq[] -- qp[i] gives the position of i in pq[] (the index j such that pq[j] is i)
+*   . Then modify the code in Algorithm 2.6 to maintain these data structures. Use the convention that pq[i] = -1 if i is
+*   not on the queue, and include a method contains() that test this condition. You need to modify the heloer methods
+*   exch() and less() but not sink() or swim().
+*
+* */
+public class Ex2_4_33_IndexPQImplementation {
+}
