05ebf81122fa5272a576a619c0a30d32d78f907f
Lizhong Yu <114469538@qq.com>
2019-03-10 18:42:44 +0800

    ? method delMin with problem

--- Chapter2_Sorting/2.4 Priority Queues/Ex2_4_34_IndexPQWithminIndexChange.java
+++ Chapter2_Sorting/2.4 Priority Queues/Ex2_4_34_IndexPQWithminIndexChange.java
@@ -1,4 +1,7 @@
 package Chapter2_4_PriorityQueues;
+
+import edu.princeton.cs.algs4.StdOut;
+
 /*
 *   Index priority-queue implementation(additional operations). Add minIndex(), change(), and delete() to your implementation
 *   of Exercise 2.4.33.
@@ -19,13 +22,17 @@ public class Ex2_4_34_IndexPQWithminIndexChange<Key extends Comparable<Key>> {
             qp[i] = -1;
         }
     }
+    // if an element is on the priority queue
     public boolean contains(int k){return qp[k] != -1;}
     public boolean isEmpty(){return n == 0;}
     public int size(){return n;}
     private void exchange(int v, int w){
         Key temp = keys[pq[v]];
+        int tempIndex = pq[v];
         keys[pq[v]] = keys[pq[w]];
+        pq[v] = pq[w];
         keys[pq[w]] = temp;
+        pq[w] = tempIndex;
     }
     private boolean less(int i, int j){
         return keys[pq[i]].compareTo(keys[pq[j]]) < 0;
@@ -49,12 +56,14 @@ public class Ex2_4_34_IndexPQWithminIndexChange<Key extends Comparable<Key>> {
     public Key min(){
         return keys[pq[1]];
     }
+    // return the min index
     public int minIndex(){return pq[1];}
     public void change(int k, Key key){
         keys[k] = key;
         swim(qp[k]);
         sink(qp[k]);
     }
+
     public void delete(int k){
         exchange(k, n--);
         swim(qp[k]);
@@ -75,17 +84,17 @@ public class Ex2_4_34_IndexPQWithminIndexChange<Key extends Comparable<Key>> {
         return indexOfMin;
     }
     public void sink(int k){
-        while (2 * k <= n){
+        while (2 * pq[k] <= n){
             // each parent node needs to compare its two children which are its left child and right child
-            int j = 2 * k;
+            int j = 2 * pq[k];
             if (j < n && less(j, j + 1)){
                 j++;
             }
-            if (less(k, j)){
+            if (less(pq[k], j)){
                 break;
             }
-            exchange(k, j);
-            k = j;
+            exchange(pq[k], j);
+            pq[k] = j;
         }
     }
     public void resize(int newSize){
@@ -103,6 +112,39 @@ public class Ex2_4_34_IndexPQWithminIndexChange<Key extends Comparable<Key>> {
     }
 
     public static void main(String[] args) {
-
+        Ex2_4_34_IndexPQWithminIndexChange indexPQWithminIndexChange = new Ex2_4_34_IndexPQWithminIndexChange(1);
+        // insert 5 items
+        indexPQWithminIndexChange.insert(1, 5);
+        indexPQWithminIndexChange.insert(2, 2);
+        indexPQWithminIndexChange.insert(3, 20);
+        indexPQWithminIndexChange.insert(4, 7);
+        indexPQWithminIndexChange.insert(5, 14);
+        for (int i = 1; i <= indexPQWithminIndexChange.size(); i++){
+            StdOut.print(indexPQWithminIndexChange.keys[i] + " ");
+        }
+        StdOut.println();
+        // return minimum index
+        StdOut.println("Minimum index: " + indexPQWithminIndexChange.minIndex());
+        // return minimum value
+        StdOut.println("Minimum value: " + indexPQWithminIndexChange.min());
+        // if the element of index is 3 is on the queue
+        StdOut.println(indexPQWithminIndexChange.contains(3));
+        // return the size of the queue
+        StdOut.println("Expected 5: " + indexPQWithminIndexChange.size());
+        // change the key related to index 1 to 6
+        indexPQWithminIndexChange.change(1, 6);
+        for (int i = 1; i <= indexPQWithminIndexChange.size(); i++){
+            StdOut.print(indexPQWithminIndexChange.keys[i] + " ");
+        }
+        StdOut.println();
+        // delete the minimum key
+        StdOut.println("Expected value: 6 -> " + indexPQWithminIndexChange.delMin());
+        // delete a key according to the provided index
+        indexPQWithminIndexChange.delete(1);
+        for (int i = 1; i <= indexPQWithminIndexChange.size(); i++){
+            StdOut.print(indexPQWithminIndexChange.keys[i] + " ");
+        }
+        StdOut.println();
+        StdOut.println("Size of queue: 3 -> " + indexPQWithminIndexChange.size());
     }
 }
