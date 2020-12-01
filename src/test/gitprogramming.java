3b6042ee81d295e9039706d21c8e19dd2eecfc92
harveybegood <harveymorning@gmail.com>
2020-12-01 01:47:11 -0800

    add new program

---
+++ src/test/gitprogramming.java
@@ -0,0 +1,9 @@
+package test;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class gitprogramming {
+    public static void main(String[] args) {
+        StdOut.println("Will this program be pushed to remote github?");
+    }
+}
