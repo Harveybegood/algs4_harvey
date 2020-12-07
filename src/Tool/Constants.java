f6b426f37e7ccd521a6a9d35d36657684b8bdbe1
harveybegood <harveymorning@gmail.com>
2020-12-06 19:38:59 -0800

    delete duplicated files of Tool

--- src/Tool/Constants.java
+++
@@ -1,6 +0,0 @@
-package Tool;
-
-public class Constants {
-    public static final String FILE_PATH = "/usr/local/algs4/src/algs4-data/";
-    public static final String unbiasedElection = "/usr/local/algs4/src/algs4-data/";
-}
