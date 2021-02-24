baf910d0d1127840f56f77a240b62cffc0a74d8f
harveybegood <harveymorning@gmail.com>
2021-02-23 23:57:34 -0800

    upload LinearProbingHashST.java

--- src/Chapter3_4_HashTables/LinearProbingHashST.java
+++ src/Chapter3_4_HashTables/LinearProbingHashST.java
@@ -10,11 +10,22 @@ public class LinearProbingHashST<Key, Value> {
         keys = (Key[]) new Object[M];
         values = (Value[]) new Object[M];
     }
+    public LinearProbingHashST(int cap){
+        LinearProbingHashST<Key, Value> linearProbingHashST;
+    }
     private int hash(Key key){
         return (key.hashCode() & 0x7fffffff) % M;
     }
-    private void resize(){
-        
+    private void resize(int cap){
+        LinearProbingHashST<Key, Value> t = new LinearProbingHashST<>(cap);
+        for (int i = 0; i < M; i++){
+            if (keys[i] != null){
+                t.put(keys[i], values[i]);
+            }
+        }
+        keys = t.keys;
+        values = t.values;
+        M = t.M;
     }
     public void put(Key key, Value value){
         if (N >= M/2){resize();}
@@ -36,5 +47,49 @@ public class LinearProbingHashST<Key, Value> {
             }
         }
         return null;
+    }
+    public boolean contains(Key key){
+        return get(key) != null;
+    }
+    public void delete(Key key){
+        if (!contains(key)){
+            return;
+        }
+        int i = hash(key);
+        while (!key.equals(keys[i])){
+            i = (i + 1)%M;
+        }
+        keys[i] = null;
+        values[i] = null;
+        i = (i + 1)%M;
+        while (keys[i] != null){
+            Key keyToRedo = keys[i];
+            Value valueToRedo = values[i];
+            N--;
+            put(keyToRedo, valueToRedo);
+            i = (i + 1)%M;
+        }
+        N--;
+        if (N > 0 && N == M/8){
+            resize();
+        }
+    }
+
+    public static void main(String[] args) {
+        LinearProbingHashST<String, Integer> linearProbingHashST = new LinearProbingHashST<>();
+        linearProbingHashST.put("S", 0);
+        linearProbingHashST.put("E", 1);
+        linearProbingHashST.put("A", 2);
+        linearProbingHashST.put("R", 3);
+        linearProbingHashST.put("C", 4);
+        linearProbingHashST.put("H", 5);
+        linearProbingHashST.put("E", 6);
+        linearProbingHashST.put("X", 7);
+        linearProbingHashST.put("A", 8);
+        linearProbingHashST.put("M", 9);
+        linearProbingHashST.put("P", 10);
+        linearProbingHashST.put("L", 11);
+        linearProbingHashST.put("E", 12);
+
     }
 }
