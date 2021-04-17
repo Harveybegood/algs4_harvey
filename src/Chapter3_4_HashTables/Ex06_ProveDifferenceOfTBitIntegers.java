c9380926d5451851791da5d25da7fc2f1a9e75cb
harveybegood <harveymorning@gmail.com>
2021-04-17 03:18:20 -0700

    upload Ex06

---
+++ src/Chapter3_4_HashTables/Ex06_ProveDifferenceOfTBitIntegers.java
@@ -0,0 +1,18 @@
+package Chapter3_4_HashTables;
+/*
+*   Suppose that keys are t-bit integers. For a modular hash function with prime M, prove that each key bit has the property
+*   that there exist two keys differing only in that bit that have different hash values.
+*
+* */
+public class Ex06_ProveDifferenceOfTBitIntegers {
+    /*
+            t-bit:
+
+            for instance, t is 4, M is 5;
+            1000(8)     %5 = 3
+            1001(9)     %5 = 4
+            1010(10)    %5 = 0
+            1100(12)    %5 = 2
+              
+    * */
+}
