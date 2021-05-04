2705102e6cef8bc59f46db205dd1f73ee8843244
harveybegood <harveymorning@gmail.com>
2021-05-04 03:24:39 -0700

    modify SeparateChainingHashST

--- src/Chapter3_4_HashTables/SeparateChainingHashST.java
+++ src/Chapter3_4_HashTables/SeparateChainingHashST.java
@@ -7,165 +7,137 @@ import edu.princeton.cs.algs4.StdOut;
 
 @SuppressWarnings("unchecked")
 public class SeparateChainingHashST<Key, Value> {
-    private static final int INIT_CAPACITY = 4;
+    public int M;                                // hash table size
+    public int n;                                // number of key-value pairs
+    private int lgM;                              //
+    private int averageListSize;                  // specify the length of each list
+    public SequentialSearchST<Key, Value>[] st;   // array of linked-list symbol tables
+    // Modules for hash
+    private int[] primes = {1, 1, 3, 7, 13, 31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381, 32749, 65521, 131071, 262139, 524287,
+            1048573, 2097143, 4194301, 8388593, 16777213, 33554393, 67108859, 134217689, 268435399, 536870909, 1073741789, 2147483647};
 
-    private int n;                                // number of key-value pairs
-    public int m;                                // hash table size
-    public SequentialSearchST<Key, Value>[] st;  // array of linked-list symbol tables
-
-
-    /**
-     * Initializes an empty symbol table.
-     */
     public SeparateChainingHashST() {
-        this(INIT_CAPACITY);
+        this(10, 5);
+    }
+    public SeparateChainingHashST(int M){
+        this.M = M;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+        for (int i = 0; i < M; i++){
+            st[i] = new SequentialSearchST<>();
+        }
     }
 
-    /**
-     * Initializes an empty symbol table with {@code m} chains.
-     * @param m the initial number of chains
-     */
-    public SeparateChainingHashST(int m) {
-        this.m = m;
-        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
-        for (int i = 0; i < m; i++)
-            st[i] = new SequentialSearchST<Key, Value>();
+    // A constructor to give the client the ability to specify the average number of probes
+    public SeparateChainingHashST(int M, int averageListSize) {
+        this.M = M;
+        this.averageListSize = averageListSize;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+        for (int i = 0; i < M; i++){
+            st[i] = new SequentialSearchST<>();
+        }
+        lgM = (int) (Math.log(M) / Math.log(2));
     }
 
     // resize the hash table to have the given number of chains,
     // rehashing all of the keys
-    private void resize(int chains) {
-        SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
-        for (int i = 0; i < m; i++) {
+    public void resize(int chains) {
+        SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<>(chains);
+        for (int i = 0; i < M; i++) {
             for (Key key : st[i].keys()) {
                 temp.put(key, st[i].get(key));
             }
         }
-        this.m  = temp.m;
+        this.M  = temp.M;
         this.n  = temp.n;
         this.st = temp.st;
     }
 
-    // hash function for keys - returns value between 0 and m-1
-    private int hashTextbook(Key key) {
-        return (key.hashCode() & 0x7fffffff) % m;
+    // hash function for keys
+    public int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % M;
     }
 
-    // hash function for keys - returns value between 0 and m-1 (assumes m is a power of 2)
-    // (from Java 7 implementation, protects against poor quality hashCode() implementations)
-    private int hash(Key key) {
-        int h = key.hashCode();
-        h ^= (h >>> 20) ^ (h >>> 12) ^ (h >>> 7) ^ (h >>> 4);
-        return h & (m-1);
+    // hash function for keys based on primes for hash table size
+    public int hashWithDesignatedModulus(Key key){
+        int t = key.hashCode() & 0x7ffffff;
+        if (lgM < 26){
+            t = t % primes[lgM + 5];
+        }
+        return t % M;
     }
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     *
-     * @return the number of key-value pairs in this symbol table
-     */
+    //Returns the number of key-value pairs in this symbol table.
     public int size() {
         return n;
     }
 
-    /**
-     * Returns true if this symbol table is empty.
-     *
-     * @return {@code true} if this symbol table is empty;
-     *         {@code false} otherwise
-     */
+    //Returns true if this symbol table is empty.
     public boolean isEmpty() {
         return size() == 0;
     }
-
-    /**
-     * Returns true if this symbol table contains the specified key.
-     *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key};
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    //Returns true if this symbol table contains the specified key.
     public boolean contains(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     }
 
-    /**
-     * Returns the value associated with the specified key in this symbol table.
-     *
-     * @param  key the key
-     * @return the value associated with {@code key} in the symbol table;
-     *         {@code null} if no such value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    //Returns the value associated with the specified key in this symbol table.
     public Value get(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to get() is null");
-        int i = hash(key);
+        int i = hashWithDesignatedModulus(key);
         return st[i].get(key);
     }
 
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+    /*Inserts the specified key-value pair into the symbol table, overwriting the old
+     value with the new value if the symbol table already contains the specified key.
+     Deletes the specified key (and its associated value) from this symbol table
+     if the specified value is {@code null}.*/
+     
     public void put(Key key, Value val) {
         if (key == null) throw new IllegalArgumentException("first argument to put() is null");
         if (val == null) {
             delete(key);
             return;
         }
-
-        // double table size if average length of list >= 10
-        if (n >= 2*m) resize(2*m);
-
-        int i = hash(key);
-        if (!st[i].contains(key)) n++;
+        // double table size if average length of list >= averageListSize
+        if ((n / M) >= averageListSize) {
+            resize(2 * M);
+            lgM++;
+        }
+        int i = hashWithDesignatedModulus(key);
+        if (!st[i].contains(key)) {
+            n++;
+        }
         st[i].put(key, val);
     }
-
-    /**
-     * Removes the specified key and its associated value from this symbol table
-     * (if the key is in this symbol table).
-     *
-     * @param  key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+     //Removes the specified key and its associated value from this symbol table
+     //(if the key is in this symbol table).
     public void delete(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to delete() is null");
 
-        int i = hash(key);
+        int i = hashWithDesignatedModulus(key);
         if (st[i].contains(key)) n--;
         st[i].delete(key);
-
-        // halve table size if average length of list <= 2
-        if (m > INIT_CAPACITY && n <= 2*m) resize(m/2);
+        // halve table size if average length of list averageListSize / 4
+        if (M > 1 && (n / M) <= averageListSize / 4){
+            resize(M/2);
+            lgM--;
+        }
     }
 
-    // return keys in symbol table as an Iterable
+    // return keys in symbol table as an iterable
     public Iterable<Key> keys() {
-        Queue<Key> queue = new Queue<Key>();
-        for (int i = 0; i < m; i++) {
+        Queue<Key> queue = new Queue<>();
+        for (int i = 0; i < M; i++) {
             for (Key key : st[i].keys())
                 queue.enqueue(key);
         }
         return queue;
     }
-
-
-    /**
-     * Unit tests the {@code SeparateChainingHashST} data type.
-     *
-     * @param args the command-line arguments
-     */
+     //Unit tests the {@code SeparateChainingHashST} data type.
+     //@param args the command-line arguments
     public static void main(String[] args) {
-        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<String, Integer>();
+        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<>();
         for (int i = 0; !StdIn.isEmpty(); i++) {
             String key = StdIn.readString();
             st.put(key, i);
