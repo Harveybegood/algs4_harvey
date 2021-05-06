46868c5366540a7fbbf353791f221d76144169cb
harveybegood <harveymorning@gmail.com>
2021-05-06 02:54:36 -0700

    Ex21_ not completed

---
+++ src/Chapter3_4_HashTables/Ex21_AverageCostSearchMiss.java
@@ -0,0 +1,9 @@
+package Chapter3_4_HashTables;
+/*
+*   Add a method to LinearProbingHashST that computes the average cost of a search miss in a table, assuming a random hash
+*   function. Note: You do not have to compute any hash functions to solve this problem.
+*
+* */
+public class Ex21_AverageCostSearchMiss<Key, Value> extends LinearProbingHashST<Key, Value> {
+    // not sure of what is that "assuming a random hash function"
+}
