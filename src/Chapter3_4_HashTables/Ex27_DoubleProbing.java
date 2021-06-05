c031618e3c0ffcf389c7aa44ae8741d92720116a
harveybegood <harveymorning@gmail.com>
2021-06-05 03:40:05 -0700

    upload Ex27_

--- src/Chapter3_4_HashTables/Ex27_DoubleProbing.java
+++ src/Chapter3_4_HashTables/Ex27_DoubleProbing.java
@@ -1,4 +1,9 @@
 package Chapter3_4_HashTables;
+
+import Chapter3_1_SymbolTables.SequentialSearchST;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
 /*
 *   Double probing. Modify SeparateChainingHashST to use a second hash function and pick the shorter of the two lists. Give
 *   a trace of the process of inserting the keys E A S Y Q U T I O N in that order into an initially empty table of size M = 3
@@ -6,7 +11,8 @@ package Chapter3_4_HashTables;
 *   as the second hash function. Give the average number of probes for random search hit and search miss in this table.
 *
 * */
-public class Ex27_DoubleProbing {
+@SuppressWarnings("unchecked")
+public class Ex27_DoubleProbing<Key, Value> {
     /*
     *   Key   Value   hash (11 K % M(3))     st[]
     *
@@ -22,21 +28,89 @@ public class Ex27_DoubleProbing {
     *   N       9       11 9 % 3 = 0
     *
     * */
-
-    // cannot understand what difference btw 11 k % 3 and 17 k % 3
-
-
-    /*
-    *    E       0       17 0 % 3 = 3
-    *    A       1       17 1 % 3 = 2
-    *    S       2       17 2 % 3 = 1
-    *    Y       3       17 3 % 3 = 0
-    *    Q       4       17 4 % 3 = 2
-    *    U       5       17 5 % 3 = 1
-    *    T       6       17 6 % 3 = 0
-    *    I       7       17 7 % 3 = 2
-    *    O       8       17 8 % 3 = 1
-    *    N       9       17 9 % 3 = 0
-    *
-    * */
+    private int M;
+    private int n;
+    public SequentialSearchST<Key, Value>[] st;
+    public Ex27_DoubleProbing(){
+        this(3);
+    }
+    public Ex27_DoubleProbing(int m){
+        this.M = m;
+        st = (SequentialSearchST<Key, Value>[])new SequentialSearchST[M];
+        for (int i = 0; i < m; i++){
+            st[i] = new SequentialSearchST<>();
+        }
+    }
+    public int hash1(Key key){
+        return (11 * key.hashCode()) % M;
+    }
+    public int hash2(Key key){
+        return (17 * key.hashCode()) % M;
+    }
+    public boolean isEmpty(){return n == 0;}
+    public int size(){return n;}
+    public boolean isContains(Key key){
+        if (key == null){throw new IllegalArgumentException("Argument key cannot be null");}
+        return get(key) != null;
+    }
+    public Value get(Key key){
+        if (key == null){throw new IllegalArgumentException("Argument key cannot be null");}
+        int i = hash1(key);
+        //if (!isContains(key)){return null;}
+        return st[i].get(key);
+    }
+    public void put(Key key, Value value){
+        if (key == null){throw new IllegalArgumentException("Argument cannot be null");}
+        if (value == null){throw new IllegalArgumentException("Argument cannot be null");}
+        if (n >= M / 2){resize(2 * M);}
+        int firstHash = hash1(key);
+        int secondHash = hash2(key);
+        int lengthOfLink1 = st[firstHash].size();
+        int lengthOfLink2 = st[secondHash].size();
+        int i;
+        if (!isContains(key)){n++;}
+        // To determine which link is longer
+        i = lengthOfLink1 < lengthOfLink2 ? firstHash : secondHash;
+        st[i].put(key, value);
+    }
+    public void resize(int newSize){
+        Ex27_DoubleProbing<Key, Value> temp = new Ex27_DoubleProbing<>(newSize);
+        for (int i = 0; i < M; i++){
+            for (Key key : st[i].keys()){
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.st = temp.st;
+        this.M = temp.M;
+        this.n = temp.n;
+    }
+    public Iterable<Key> keys(){
+        Queue<Key> queue = new Queue<>();
+        for (int i = 0; i < M; i++){
+            for (Key key : st[i].keys()){
+                queue.enqueue(key);
+            }
+        }
+        return queue;
+    }
+    // Method delete() will be written on Ex29
+    public void delete(Key key){}
+    public static void main(String[] args) {
+        Ex27_DoubleProbing<String, Integer> doubleProbing = new Ex27_DoubleProbing<>();
+        doubleProbing.put("E", 0);
+        doubleProbing.put("X", 1);
+        doubleProbing.put("A", 2);
+        doubleProbing.put("M", 3);
+        doubleProbing.put("P", 4);
+        doubleProbing.put("L", 5);
+        doubleProbing.put("Q", 6);
+        doubleProbing.put("U", 7);
+        doubleProbing.put("I", 8);
+        doubleProbing.put("O", 9);
+        doubleProbing.put("N", 10);
+        doubleProbing.put("S", 11);
+        for (String s : doubleProbing.keys()){
+            StdOut.println(s + "   " + doubleProbing.get(s));
+        }
+    }
 }
