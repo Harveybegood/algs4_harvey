7dae5b2471e87f15ff7b6c317112ae841388b901
harveybegood <harveymorning@gmail.com>
2021-06-29 23:29:02 -0700

    not finished

---
+++ src/Chapter3_4_HashTables/Ex35_Chi_squareTest.java
@@ -0,0 +1,9 @@
+package Chapter3_4_HashTables;
+/*
+*   Chi-square test. Use your solution from Ex3.4.30 to check the assumption that the hash function for commonly-used key
+*   types produces random values.
+*
+* */
+public class Ex35_Chi_squareTest {
+
+}
